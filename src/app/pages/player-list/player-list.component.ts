84bfc3f8a476df6a84db7fca1070ee73df647a75
Pablo Souza <pablo.osouza@outlook.com>
2022-08-21 08:55:30 -0300

    chore: player screen

--- src/app/pages/player-list/player-list.component.ts
+++ src/app/pages/player-list/player-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationBehaviorOptions, Router } from '@angular/router';
+import { IPlayer } from 'src/app/shared/interfaces/IPlayer';
+import { PlayerService } from 'src/app/shared/services/player.service';
 
 @Component({
   selector: 'app-player-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private playerService: PlayerService) { }
 
   ngOnInit(): void {
+    this.getPlayer();
   }
 
+  isLoadingTable: boolean = true;
+  playerList: IPlayer[] = [];
+
+  deletePlayer(id: number) {
+
+  }
+
+  getPlayer() {
+    this.playerService.getAllPlayers().subscribe(
+      (response: IPlayer[]) => {
+        this.playerList = response;
+        this.isLoadingTable = false;
+      });
+  }
+
+  redirectToWithParams(url: string, id: number) {
+    const dataParams: NavigationBehaviorOptions = {
+      state: {
+        id: id
+      }
+    };
+    this.router.navigate([`${url}/`], dataParams)
+  }
 }
--- src/app/pages/player-list/player-list.module.ts
+++ src/app/pages/player-list/player-list.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerListComponent } from './player-list.component';
 import { PlayerListRoutingModule } from './player-list-routing.module';
+import { DfWrapperModule } from 'src/app/features/df-wrapper/df-wrapper.module';
+import { DfLoaderModule } from 'src/app/shared/components/df-loader/df-loader.module';
+import { DfButtonModule } from 'src/app/shared/components/df-button/df-button.module';
 
 
 
@@ -11,7 +14,10 @@ import { PlayerListRoutingModule } from './player-list-routing.module';
   ],
   imports: [
     CommonModule,
-    PlayerListRoutingModule
+    PlayerListRoutingModule,
+    DfWrapperModule,
+    DfLoaderModule,
+    DfButtonModule
   ]
 })
 export class PlayerListModule { }
